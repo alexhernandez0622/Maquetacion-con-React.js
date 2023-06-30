93150a285d745d4a2299b8e26761820af5a27d89
Alex Hernández <alexanderhernandez0622@gmail.com>
2023-06-29 22:39:01 -0500

    cambios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  // Estado para controlar la visibilidad del menú desplegable
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  // Función para alternar la visibilidad del menú desplegable
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <nav className="navbar">
+        <div className="logo">Logo</div>
+        <div className={`menu ${isMenuOpen ? 'open' : ''}`}>
+          <ul>
+            <li>Inicio</li>
+            <li>Acerca de</li>
+            <li>Servicios</li>
+            <li>Contacto</li>
+          </ul>
+        </div>
+        <div className="hamburger" onClick={toggleMenu}>
+          <div className={`bar ${isMenuOpen ? 'open' : ''}`}></div>
+          <div className={`bar ${isMenuOpen ? 'open' : ''}`}></div>
+          <div className={`bar ${isMenuOpen ? 'open' : ''}`}></div>
+        </div>
+      </nav>
+      <section className="content">
+        <h1>Bienvenido</h1>
+        <p>Este es un sitio web dinámico e innovador.</p>
+      </section>
     </div>
   );
-}
+};
 
 export default App;
